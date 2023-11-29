02ca20fefe8daf9b09a84cb32c52614468b6e40b
Onifade Joshua <onifadjosh@gmail.com>
2023-11-29 08:03:39 +0100

    file path for background

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["./*.{html,js}"],
   theme: {
+    backgroundImage: {
+      'bg1': "url('./images/bg1.png')",
+      'bg2': "url('./images/img05.png')",
+      'bg3': "url('.",
+      
+    },
+
     extend: {},
   },
   plugins: [],
