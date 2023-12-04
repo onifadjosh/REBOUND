dabd186ad37b7e61f35f0ff8b09d82ee26dc1fc0
Onifade Joshua <onifadjosh@gmail.com>
2023-12-04 10:43:22 +0100

    menu change

--- src/main.js
+++ src/main.js
@@ -1,1 +1,29 @@
-const disp1 = document.getElementById()
+// const disp1 = document.getElementsByClassName("hiddenElement");
+// const togelem = document.getElementsByClassName('tog1');
+
+
+// disp1.addEventListener( 'mouseover', () =>{
+//     togelem.style.display = "block";
+// }
+
+// );
+
+// disp1.addEventListener('mouseout', () =>{
+//     togelem.style.display = "hidden";
+// }
+// );
+
+
+        // Get the trigger and hidden elements
+        const triggerElement = document.getElementById('trigger-element');
+        const hiddenElement = document.getElementById('hidden-element');
+
+        // Add event listener for hover
+        triggerElement.addEventListener('mouseover', () => {
+            hiddenElement.style.display = 'flex';
+        });
+
+        triggerElement.addEventListener('mouseout', () => {
+            hiddenElement.style.display = 'none';
+        });
+
