791d341bb61770641573cdd7df43bfb61f1344a8
Onifade Joshua <onifadjosh@gmail.com>
2023-12-09 00:46:33 +0100

    responsive menu changes

---
+++ src/menu.js
@@ -0,0 +1,25 @@
+const closeBtn = document.getElementById('close-btn');
+const menuBtn = document.getElementById('menu-btn');
+const menuItems = document.getElementById('menu-items');
+
+
+menuBtn.addEventListener('click', () => {
+    menuItems.style.display= 'flex';
+} );
+
+closeBtn.addEventListener('click', () => {
+    menuItems.style.display= 'none'
+} );
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    window.addEventListener('scroll', function () {
+      var menuHead = document.getElementById('menuHead');
+  
+      if (window.scrollY > menuHead.offsetTop) {
+        menuHead.classList.add('fixed');
+      } else {
+        menuHead.classList.remove('fixed');
+      }
+    });
+  });
