6cd998000a8ba79388ff1c0db0cb5adf5bfb8e37
Onifade Joshua <onifadjosh@gmail.com>
2023-12-11 20:50:53 +0100

    update on the menu

--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
-    // Get the trigger and hidden elements
-    // const trigger = ['trigger-element', 'trigger-element1'];
-    // const selectedArray = Array.from(document.querySelectorAll(idsToSelect.map(id => `#${id}`)))
+    //use of DOM to get the ID needed 
     const triggerElement = document.getElementById('trigger-element');
     const hiddenElement = document.getElementById('hidden-element');
     const clicke = document.getElementById('clicke');
@@ -8,25 +6,27 @@
 
     const triggerElement1 = document.getElementById('trigger-element1');
     const hiddenElement1 = document.getElementById('hidden-element1');
+    const clicke1 = document.getElementById('clicke1');
 
     const triggerElement2 = document.getElementById('trigger-element2');
     const hiddenElement2 = document.getElementById('hidden-element2');
+    const clicke2 = document.getElementById('clicke2');
 
     const triggerElement3 = document.getElementById('trigger-element3');
     const hiddenElement3 = document.getElementById('hidden-element3');
+    const clicke3 = document.getElementById('clicke3');
     
     const triggerElement4 = document.getElementById('info-trig');
     const hiddenElement4 = document.getElementById('info-hidden');
+    const clicke4 = document.getElementById('clicke4');
 
-    // Add event listener for hover
+    // Add event listener for hover especially for the large screen menu
     triggerElement.addEventListener('mouseover', () => {
         hiddenElement.style.display = 'block';
     });
 
-    // triggerElement.addEventListener('click', ()=>{
-    //     clicke.style.display = 'block'
-    // });
-
+    
+    // click event listener for menu in small screens (new)
     triggerElement.addEventListener('click', ()=>{
         if(clicke.style.display === 'block'){
             clicke.style.display = 'none';
@@ -39,12 +39,9 @@
         hiddenElement.style.display = 'none';
     });
 
-    // Add event listener for hover on the hidden element
+    // Add event listener for hover on the hidden element to stay on block and and hidden when not on hover
     hiddenElement.addEventListener('mouseover', () => {
         hiddenElement.style.display = 'block';
-        // triggerElement.style.border = '1px solid black';
-        // triggerElement.style.borderRadius = '40px';
-        
     });
 
     hiddenElement.addEventListener('mouseout', () => {
@@ -54,7 +51,7 @@
     
     // break for the second hover
 
-    // Add event listener for hover
+    // Add event listener for hover especially for the large screen menu ##2
     triggerElement1.addEventListener('mouseover', () => {
         hiddenElement1.style.display = 'block';
     });
@@ -66,13 +63,19 @@
     // Add event listener for hover on the hidden element
     hiddenElement1.addEventListener('mouseover', () => {
         hiddenElement1.style.display = 'block';
-        // triggerElement.style.border = '1px solid black';
-        // triggerElement.style.borderRadius = '40px';
-        
     });
 
     hiddenElement1.addEventListener('mouseout', () => {
         hiddenElement1.style.display = 'none';
+    });
+
+     // click event listener for menu in small screens (red)
+     triggerElement1.addEventListener('click', ()=>{
+        if(clicke1.style.display === 'block'){
+            clicke1.style.display = 'none';
+        } else {
+            clicke1.style.display = 'block';
+        }
     });
 
     //break for third hover
@@ -95,6 +98,15 @@
 
     hiddenElement2.addEventListener('mouseout', () => {
         hiddenElement2.style.display = 'none';
+    });
+
+    // click event listener for menu in small screens (white)
+    triggerElement2.addEventListener('click', ()=>{
+        if(clicke2.style.display === 'block'){
+            clicke2.style.display = 'none';
+        } else {
+            clicke2.style.display = 'block';
+        }
     });
 
 
@@ -120,6 +132,14 @@
         hiddenElement3.style.display = 'none';
     });
 
+    triggerElement3.addEventListener('click', ()=>{
+        if(clicke3.style.display === 'block'){
+            clicke3.style.display = 'none';
+        } else {
+            clicke3.style.display = 'block';
+        }
+    });
+
     //break for the fifth hover menu
     triggerElement4.addEventListener('mouseover', () => {
         hiddenElement4.style.display = 'block';
@@ -136,4 +156,12 @@
 
     hiddenElement4.addEventListener('mouseout', () =>{
         hiddenElement4.style.display = 'none';
+    });
+
+    triggerElement4.addEventListener('click', ()=>{
+        if(clicke4.style.display === 'block'){
+            clicke4.style.display = 'none';
+        } else {
+            clicke4.style.display = 'block';
+        }
     });
